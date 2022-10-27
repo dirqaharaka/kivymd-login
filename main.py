31b9b43000623cce521826b5c8c0c4942eecd6d5
dirkaharaka99 <dirkaharaka77@gmail.com>
2022-10-27 16:49:25 +0800

    login page

--- main.py
+++ main.py
@@ -12,10 +12,17 @@ kivy.require('1.0.8')
 Window.size = (350, 580)
 class LoginApp(MDApp):
     def build(self):
-        self.manager = ScreenManager(transition = NoTransition())
-        self.manager.add_widget(Builder.load_file("pre-splash.kv"))
-        return self.manager 
+        global screen_manager
+        screen_manager = ScreenManager()
+        # self.manager = ScreenManager(transition = NoTransition())
+        screen_manager.add_widget(Builder.load_file("pre-splash.kv"))
+        screen_manager.add_widget(Builder.load_file("login.kv"))
+        return screen_manager 
 
+    def on_start(self):
+        Clock.schedule_once(self.login, 7)
 
+    def login(self, *args):
+        screen_manager.current = "login"
 if __name__== '__main__':
     LoginApp().run()
